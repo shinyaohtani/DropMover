d7ab7fd9ecb43ab0f023546cc3c3326d37a62751
Shinya Ohtani <shinya_ohtani@yahoo.co.jp>
2025-06-02 16:18:06 +0900

    Initial Commit

---
+++ DropMover/DropMoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropMoverApp.swift
+//  DropMover
+//
+//  Created by 大谷伸弥 on 2025/06/02.
+//
+
+import SwiftUI
+
+@main
+struct DropMoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DropMoverTests/DropMoverTests.swift
@@ -0,0 +1,17 @@
+//
+//  DropMoverTests.swift
+//  DropMoverTests
+//
+//  Created by 大谷伸弥 on 2025/06/02.
+//
+
+import Testing
+@testable import DropMover
+
+struct DropMoverTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
